8a2af4dbd71db2017dc16bfb7bde3d6d5c40a212
Alogon02 <alfonso.lopez.gonzalez@udc.es>
2021-02-17 16:39:45 +0100

    Alfonso

---
+++ tutorial/temp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+nombre= input('Escribe tu nombre: ')
+edad= input('Escribe tu edad: ')
+print('¡Buenos días ' + nombre + ', disfrute de sus ' + edad + ' años!')
+
+
